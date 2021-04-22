5f9ffbb37eedd7cf74dfba20c511971878cf6acd
John Shiver <johneshiver@gmail.com>
2021-04-22 13:23:37 -0400

    refactor tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-use crate::token::Token;
+use crate::token::{lookup_ident, Token};
 use std::iter::Peekable;
 use std::str::Chars;
 
@@ -25,6 +25,15 @@ impl<'a> Lexer<'a> {
             Some(',') => Token::Comma,
             Some(';') => Token::Semicolon,
             Some('=') => Token::Assign,
+            Some(ch) => {
+                if ch.is_alphabetic() {
+                    let ident = self.read_identifier(ch);
+                    let tok = lookup_ident(ident);
+                    tok;
+                }
+                Token::Illegal
+            }
+
             None => Token::Eof,
             _ => Token::Illegal,
         }
@@ -42,6 +51,23 @@ impl<'a> Lexer<'a> {
 
     fn read_char(&mut self) -> Option<char> {
         self.input.next()
+    }
+
+    fn peek_char(&mut self) -> Option<&char> {
+        self.input.peek()
+    }
+
+    fn read_identifier(&mut self, ch: char) -> String {
+        let mut ident = String::new();
+        ident.push(ch);
+        while let Some(&ch) = self.input.peek() {
+            if ch.is_alphabetic() {
+                ident.push(self.read_char().expect("read beyond end of input"));
+            } else {
+                break;
+            }
+        }
+        ident
     }
 }
 
@@ -70,165 +96,59 @@ mod tests {
         }
     }
 
-    //     #[test]
-    //     fn next_token_multi_line() {
-    //         let input = "let five = 5;
-    // let ten = 10;
-    //
-    // let add = fn(x, y) {
-    //   x + y;
-    // };
-    //
-    // let result = add(five, ten);
-    // ";
-    //     let expected_tokens = vec![
-    //         Token::Let,
-    //         Token {
-    //             literal: "five".to_string(),
-    //             kind: TokenType::Ident,
-    //         },
-    //         Token {
-    //             literal: "=".to_string(),
-    //             kind: TokenType::Assign,
-    //         },
-    //         Token {
-    //             literal: ";".to_string(),
-    //             kind: TokenType::Semicolon,
-    //         },
-    //         Token {
-    //             literal: "let".to_string(),
-    //             kind: TokenType::Let,
-    //         },
-    //         Token {
-    //             literal: "ten".to_string(),
-    //             kind: TokenType::Ident,
-    //         },
-    //         Token {
-    //             literal: "=".to_string(),
-    //             kind: TokenType::Assign,
-    //         },
-    //         Token {
-    //             literal: "10".to_string(),
-    //             kind: TokenType::Int,
-    //         },
-    //         Token {
-    //             literal: ";".to_string(),
-    //             kind: TokenType::Semicolon,
-    //         },
-    //         Token {
-    //             literal: "let".to_string(),
-    //             kind: TokenType::Let,
-    //         },
-    //         Token {
-    //             literal: "add".to_string(),
-    //             kind: TokenType::Ident,
-    //         },
-    //         Token {
-    //             literal: "=".to_string(),
-    //             kind: TokenType::Assign,
-    //         },
-    //         Token {
-    //             literal: "fn".to_string(),
-    //             kind: TokenType::Function,
-    //         },
-    //         Token {
-    //             literal: "(".to_string(),
-    //             kind: TokenType::Lparen,
-    //         },
-    //         Token {
-    //             literal: "x".to_string(),
-    //             kind: TokenType::Ident,
-    //         },
-    //         Token {
-    //             literal: ",".to_string(),
-    //             kind: TokenType::Comma,
-    //         },
-    //         Token {
-    //             literal: "y".to_string(),
-    //             kind: TokenType::Ident,
-    //         },
-    //         Token {
-    //             literal: ")".to_string(),
-    //             kind: TokenType::Rparen,
-    //         },
-    //         Token {
-    //             literal: "{".to_string(),
-    //             kind: TokenType::Rbrace,
-    //         },
-    //         Token {
-    //             literal: "x".to_string(),
-    //             kind: TokenType::Ident,
-    //         },
-    //         Token {
-    //             literal: "+".to_string(),
-    //             kind: TokenType::Plus,
-    //         },
-    //         Token {
-    //             literal: "y".to_string(),
-    //             kind: TokenType::Ident,
-    //         },
-    //         Token {
-    //             literal: ";".to_string(),
-    //             kind: TokenType::Semicolon,
-    //         },
-    //         Token {
-    //             literal: "}".to_string(),
-    //             kind: TokenType::Rbrace,
-    //         },
-    //         Token {
-    //             literal: ";".to_string(),
-    //             kind: TokenType::Semicolon,
-    //         },
-    //         Token {
-    //             literal: "let".to_string(),
-    //             kind: TokenType::Let,
-    //         },
-    //         Token {
-    //             literal: "result".to_string(),
-    //             kind: TokenType::Ident,
-    //         },
-    //         Token {
-    //             literal: "=".to_string(),
-    //             kind: TokenType::Assign,
-    //         },
-    //         Token {
-    //             literal: "add".to_string(),
-    //             kind: TokenType::Ident,
-    //         },
-    //         Token {
-    //             literal: "(".to_string(),
-    //             kind: TokenType::Lparen,
-    //         },
-    //         Token {
-    //             literal: "five".to_string(),
-    //             kind: TokenType::Ident,
-    //         },
-    //         Token {
-    //             literal: ",".to_string(),
-    //             kind: TokenType::Comma,
-    //         },
-    //         Token {
-    //             literal: "ten".to_string(),
-    //             kind: TokenType::Ident,
-    //         },
-    //         Token {
-    //             literal: ")".to_string(),
-    //             kind: TokenType::Rparen,
-    //         },
-    //         Token {
-    //             literal: ";".to_string(),
-    //             kind: TokenType::Semicolon,
-    //         },
-    //         Token {
-    //             literal: "\0".to_string(),
-    //             kind: TokenType::Eof,
-    //         },
-    //     ];
-    //     let lexer_input = input.to_string();
-    //     let mut lexer = Lexer::new(lexer_input);
-    //     for token in expected_tokens {
-    //         let next_token = lexer.next_token();
-    //         assert_eq!(token, next_token);
-    //     }
-    // }
+    #[test]
+    fn next_token_multi_line() {
+        let input = "let five = 5;
+    let ten = 10;
+    
+    let add = fn(x, y) {
+      x + y;
+    };
+    
+    let result = add(five, ten);
+    ";
+        let expected_tokens = vec![
+            Token::Let,
+            Token::Ident("five".to_string()),
+            Token::Assign,
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident("ten".to_string()),
+            Token::Assign,
+            Token::Int(10),
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident("add".to_string()),
+            Token::Assign,
+            Token::Function,
+            Token::Lparen,
+            Token::Ident("x".to_string()),
+            Token::Comma,
+            Token::Ident("y".to_string()),
+            Token::Rparen,
+            Token::Rbrace,
+            Token::Ident("x".to_string()),
+            Token::Plus,
+            Token::Ident("y".to_string()),
+            Token::Semicolon,
+            Token::Rbrace,
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident("result".to_string()),
+            Token::Assign,
+            Token::Ident("add".to_string()),
+            Token::Lparen,
+            Token::Ident("five".to_string()),
+            Token::Comma,
+            Token::Ident("ten".to_string()),
+            Token::Rparen,
+            Token::Semicolon,
+            Token::Eof,
+        ];
+        let mut lexer = Lexer::new(input);
+        for token in expected_tokens {
+            let next_token = lexer.next_token();
+            assert_eq!(token, next_token);
+        }
+    }
 }
--- src/token.rs
+++ src/token.rs
@@ -26,28 +26,10 @@ pub enum Token {
     Let,
 }
 
-//
-// // Constants --------------------------------------------
-//
-// const ILLEGAL: &str = "ILLEGAL";
-// const EOF: &str = "EOF";
-//
-// // Identifiers and literals
-// const IDENT: &str = "IDENT"; // add, foobar, x, y, ...
-// const INT: &str = "INT"; // 134356
-//
-// // Operators
-// const ASSIGN: &str = "=";
-// const PLUS: &str = "+";
-//
-// // Delimiters
-// const COMMA: &str = ",";
-// const SEMICOLON: &str = ";";
-// const LPAREN: &str = "(";
-// const RPAREN: &str = ")";
-// const LBRACE: &str = "{";
-// const RBRACE: &str = "}";
-//
-// // Keywords
-// const FUNCTION: &str = "FUNCTION";
-// const LET: &str = "LET";
+pub fn lookup_ident(ident: String) -> Token {
+    match ident.as_str() {
+        "let" => Token::Let,
+        "fn" => Token::Function,
+        _ => Token::Ident(ident),
+    }
+}
