04345b56980e7a5e986563c23159658e5d2c4838
John Shiver <johneshiver@gmail.com>
2021-04-23 14:21:41 -0400

    working repl

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+use crate::repl::start;
+use std::io::{stdin, stdout};
+
 mod lexer;
 mod repl;
 mod token;
 
-fn main() {}
+fn main() {
+    let input = stdin();
+    let output = stdout();
+    start(&mut input.lock(), &mut output.lock());
+}
